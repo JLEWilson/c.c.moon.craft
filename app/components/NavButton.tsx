ffad02259fb753c7bb5810e448ecd1523d1e3a2f
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-01 15:32:14 -0800

    add media query sizing

--- app/components/NavButton.tsx
+++ app/components/NavButton.tsx
@@ -8,17 +8,39 @@ import { join } from "~/utils"
 type NavButtonProps = {
   to: string
   text: string
+  width?: number
 }
-const NavButton: FC<NavButtonProps> = ({ to, text }) => {
-  const wrapperStyles = [...centered, `mx-4`, `w-full`, `h-full`]
-  const activeStyles = [`bg-[#fcf9e6]`]
-  const inactiveStyles = [`bg-[#dde2b9]`, `py-3`, `px-16`]
+const NavButton: FC<NavButtonProps> = ({ to, text, width }) => {
+  const wrapperStyles = [
+    ...centered,
+    `w-full`,
+    `mx-1`,
+    `sm:mx-[2px]`,
+    `md:mx-[1vw]`,
+    `lg:mx-[3vw]`,
+  ]
+  const generalStyles = [
+    `transition`,
+    `ease-in-out`,
+    `py-3`,
+    `px-4`,
+    `w-1/${width}`,
+    `rounded`,
+    `shrink-0`,
+    `hover:bg-[#b7daf2]`,
+    `active:bg-[#a07e39]`,
+    `duration-500`,
+  ]
+  const activeStyles = [`bg-[#a07e39]`]
+  const inactiveStyles = [`bg-[#a14c0b]`]
   return (
     <div className={join(...wrapperStyles)}>
       <NavLink
         to={to}
         className={({ isActive }) =>
-          isActive ? join(...activeStyles) : join(...inactiveStyles)
+          isActive
+            ? join(...generalStyles, ...activeStyles)
+            : join(...generalStyles, ...inactiveStyles)
         }
       >
         {text}
