f94e2b22d4e9b3425a0d09a925406030c4d3ba10
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-01 15:35:50 -0800

    add custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: [`./app/**/*.{ts,tsx,jsx,js}`],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fade: {
+          "50%": { color: `rgb(0,0,0)` },
+        },
+      },
+      animation: {
+        "spin-slow": `spin 15s linear infinite`,
+        "bounce-slow": `bounce 1s ease-in-out infinite`,
+        "pulse-slow": `pulse 15s`,
+        "fade-slow": `fade 30s ease-in-out infinite`,
+      },
+    },
   },
   plugins: [],
 }
