ebb5b3e0ad94072f7f15d2d2f1bb5a1fb41641c6
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-01 15:32:37 -0800

    add media query sizing and animation testing

--- app/routes/navbar.tsx
+++ app/routes/navbar.tsx
@@ -1,11 +1,13 @@
 import type { FC } from "react"
 
+import { Form } from "remix"
+
 import NavButton from "~/components/NavButton"
-import { centered } from "~/styles/tailwind_templates"
-import { join } from "~/utils"
+import { centered, header } from "~/styles/tailwind_templates"
+import { join, useOptionalUser } from "~/utils"
 const ROUTES = [
   {
-    to: `/home`,
+    to: `/`,
     text: `Home`,
   },
   {
@@ -21,21 +23,101 @@ const ROUTES = [
     text: `Contact`,
   },
 ]
-
+const USER_ROUTES = {
+  login: {
+    to: `/login`,
+    text: `login`,
+  },
+  logout: {
+    to: `/logout`,
+    text: `Logout`,
+  },
+}
+type UserButtonProps = {
+  width: number
+}
+const UserButton: FC<UserButtonProps> = ({ width }) => {
+  const user = useOptionalUser()
+  const shared = [`fixed`, `sm:static`, `top-2`, `right-6`]
+  if (user) {
+    return (
+      <div className={join(...shared, `ml-auto`, `sm:mr-2`, `shrink-0`)}>
+        <Form action="/logout" method="post" className={join(`ml-auto`)}>
+          <button
+            type="submit"
+            className={join(
+              `transition`,
+              `ease-in-out`,
+              `py-3`,
+              `px-4`,
+              `w-1/${width}`,
+              `rounded`,
+              `shrink-0`,
+              `bg-[#a14c0b]`,
+              `hover:bg-[#b7daf2]`,
+              `active:bg-[#a07e39]`,
+              `duration-500`,
+            )}
+          >
+            Log Out
+          </button>
+        </Form>
+      </div>
+    )
+  }
+  return (
+    <div className={join(...shared)}>
+      <NavButton
+        key={USER_ROUTES.login.to}
+        to={USER_ROUTES.login.to}
+        text={USER_ROUTES.login.text}
+        width={width}
+      />
+    </div>
+  )
+}
 const NavBar: FC = () => {
   const container = [
-    `fixed`,
-    `bottom-0`,
+    `sticky`,
+    `top-0`,
     `w-full`,
-    `bg-[#2F3E46DE]`,
-    `pt-2.5`,
-    `pb-4`,
+    `bg-[#71816e]`,
+    `py-3`,
+    `px-1`,
+    `sm:px-4`,
+    `gap-4`,
+    `sm:gap-0`,
+    `md:gap-4`,
+    `flex-col`,
+    `sm:flex-row`,
   ]
+
   return (
     <div className={join(...container, ...centered)}>
-      {ROUTES.map((route) => {
-        return <NavButton key={route.to} to={route.to} text={route.text} />
-      })}
+      <h1
+        className={join(
+          ...header,
+          `mr-auto`,
+          `ml-3`,
+          `sm:ml-0`,
+          `animate-fade-slow`,
+        )}
+      >
+        C.C.Moon.Craft
+      </h1>
+      <div className={join(...centered, `mx-auto`)}>
+        {ROUTES.map((route) => {
+          return (
+            <NavButton
+              key={route.to}
+              to={route.to}
+              text={route.text}
+              width={ROUTES.length + Object.keys(USER_ROUTES).length}
+            />
+          )
+        })}
+      </div>
+      <UserButton width={ROUTES.length + Object.keys(USER_ROUTES).length} />
     </div>
   )
 }
