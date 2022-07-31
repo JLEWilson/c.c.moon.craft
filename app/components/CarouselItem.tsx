62624ba427ea4ba5960c1858d903f59d833b7f90
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-07-30 22:34:05 -0800

    inherit styles

--- app/components/CarouselItem.tsx
+++ app/components/CarouselItem.tsx
@@ -2,13 +2,17 @@ import type { FC } from "react"
 import React from "react"
 
 import type { Item } from "@prisma/client"
+
+import { join } from "~/utils"
+
 type CarouselItemProps = {
   item: Item
+  styles: string[]
 }
-const CarouselItem: FC<CarouselItemProps> = ({ item }) => {
-  const styles = [`block `, `text-center`, `h-[200px]`]
+const CarouselItem: FC<CarouselItemProps> = ({ item, styles }) => {
+  const baseStyles = [`block `, `text-center`, `h-[200px]`]
   return (
-    <div>
+    <div className={join(...baseStyles, ...styles)}>
       <h1>{item.name}</h1>
       <h2>{item.price}</h2>
       {item.imagePath && <img src={item.imagePath} />}
